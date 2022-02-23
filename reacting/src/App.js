3ac2a032a2777cdbca37d6772818d9d3f9334d96
itzmyhub <italo.machado@mail.uft.edu.br>
2022-02-22 21:34:14 -0300

    css global e css em componentes.

--- reacting/src/App.js
+++ reacting/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
   var nome = "Zorro"
 
   return (
     <div className="App">
+      <h1>IHH DOIDAO</h1>
+      <Frase/>
       <SayMyName nome="Jack"/>
       <SayMyName nome="Ítalo"/>
       <SayMyName nome={nome}/>
