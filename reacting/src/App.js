a7cf52cf804d7d0dbf61086534a453fb799b3f11
itzmyhub <italo.machado@mail.uft.edu.br>
2022-02-28 03:12:44 -0300

    renderização condicional: usando condições para mostrar/remover renders

--- reacting/src/App.js
+++ reacting/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
-import Evento from './components/Evento';
-import Form from './components/Form';
+import Condicional from './components/Condicional';
 
 function App() {
 
   return (
     <div className="App">
-      <h1>IHH DOIDAO</h1>
-      <Evento numero="1"/>
-      <Form/>
+      <h1>Renderização Condicional:</h1>
+      <Condicional/>
     </div>
   );
 }
---
+++ reacting/src/components/Condicional.js
@@ -0,0 +1,34 @@
+import {useState} from 'react'
+
+function Condicional() {
+
+    const [email, setEmail] = useState()
+    const [userEmail, setUserEmail] = useState()
+
+    function enviarEmail(e) {
+        e.preventDefault()
+        setUserEmail(email)
+    }
+
+    function limparEmail() {
+        setUserEmail('')
+    }
+
+    return (
+        <div>
+            <h2>Cadastre o seu e-mail:</h2>
+            <form>
+                <input type="email" placeholder="Digite o seu e-mail..." onChange={(e) => setEmail(e.target.value)}/>
+                <button onClick={enviarEmail}>Enviar email</button>
+            </form>
+            {userEmail && (
+                <div>
+                    <p>O email do usuário atual é: {userEmail}</p>
+                    <button onClick={limparEmail}>Limpar email</button>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Condicional
