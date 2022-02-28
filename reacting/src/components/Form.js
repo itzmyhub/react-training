4343567e1c4012bd9f8e5edcf3f3d21ec3c46552
itzmyhub <italo.machado@mail.uft.edu.br>
2022-02-28 02:31:41 -0300

    hook para alterar o estado e valores em eventos

--- reacting/src/components/Form.js
+++ reacting/src/components/Form.js
@@ -1,16 +1,28 @@
+import { useState } from 'react'
+
 function Form() {
 
     function cadastrarUsuario(e) {
         e.preventDefault()
+        console.log(name)
+        console.log(password)
         console.log("Usuário Cadastrado")
     }
+
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
 
     return (
         <div>
             <h1>Meu Formulário:</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Seu nome..."/>
+                    <label htmlFor="name">Nome:</label>
+                    <input type="text" id="name" name="name" placeholder="Seu nome..." onChange={(e) => setName(e.target.value)}/>
+                </div>
+                <div>
+                    <label htmlFor="password">Senha:</label>
+                    <input type="password" id="password" name="password" placeholder="Sua senha..." onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 <div>
                     <input type="submit" placeholder="Confirmar"/>
